7ebe13402ff4cc858116b7e9f066fbba16dfe020
Ewan Burns <ewan@beezup.com>
2019-09-24 16:30:06 +0200

    Formatting file

--- sample-server/server.ts
+++ sample-server/server.ts
@@ -1,12 +1,12 @@
 import { serve } from "https://deno.land/std/http/server.ts";
 
-const host: string = "localhost"
+const host: string = "localhost";
 const port: number = 2312;
 
 const body = new TextEncoder().encode("Hello World\n");
 const s = serve(`${host}:${port}`);
 
-console.log(`Listening at ${host} on ${port}`)
+console.log(`Listening at ${host} on ${port}`);
 
 window.onload = async () => {
   for await (const req of s) {
