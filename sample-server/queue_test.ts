8f49b4593725ce60e5692732ba31f6c79f3e724d
Ewan Burns <ewan@beezup.com>
2020-04-20 12:43:02 +0200

    new files i forgot, does -a not work anymore ?

---
+++ sample-server/queue_test.ts
@@ -0,0 +1,8 @@
+import { runTests, test } from "https://deno.land/std/testing/mod.ts";
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+
+test({
+    name: 
+});
+
+runTests();
